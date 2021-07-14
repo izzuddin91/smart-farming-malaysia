3a6528d807f5bad89d64bd3aac99cccc29696b5c
izzuddin991 <izzuddin.muhamad@yahoo.com>
2021-07-14 16:08:34 +0800

    push

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static('./package.json'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: './src/'});});
+
+// app.listen(process.env.PORT || 8080);
+
+//Install express server
 const express = require('express');
+
+
 const app = express();
 
-app.use(express.static('./package.json'));
+// app.use(express.static('./package.json'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './src/'});});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/smart-farming-malaysia'));
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/smart-farming-malaysia/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
