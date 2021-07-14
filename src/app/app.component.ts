2c0d65832394da68c7b5d9aaccd6581b2a2b196c
izzuddin991 <izzuddin.muhamad@yahoo.com>
2021-07-14 15:29:29 +0800

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smart-farming-malaysia';
+
+  constructor( private readonly router: Router){
+
+  }
+
+  aboutPage(){
+    this.router.navigate(['/about'], { state: { updatingUser: false } });
+  }
+
+  contactPage(){
+    this.router.navigate(['/contact'], { state: { updatingUser: false } });
+  }
+
+  architecturePage(){
+    this.router.navigate(['/architecture'], { state: { updatingUser: false } });
+  }
+
+  mediaPage(){
+    this.router.navigate(['/media'], { state: { updatingUser: false } });
+  }
+
+  homePage(){
+    this.router.navigate([''], { state: { updatingUser: false } });
+  }
 }
