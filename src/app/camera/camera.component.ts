64994ae0dadd4a9f6008dd654585fd969e65938c
izzuddin991 <izzuddin.muhamad@yahoo.com>
2021-07-18 17:28:04 +0800

    push 18 july - tarik data guna angular

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import firebase from 'firebase';
+import {saveAs as importedSaveAs} from "file-saver";
 
 @Component({
   selector: 'app-camera',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraComponent implements OnInit {
 
-  constructor() { }
+  time : String =  ''
+  camera_status : String =  ''
+  camera_timestamp : String = ''
+  image: any
+  app : any
+  constructor(private renderer: Renderer2) {
+    
+    if (!firebase.apps.length) {
+      this.app =  firebase.initializeApp({ 
+        apiKey: "AIzaSyDFV8JWL4z4zvkT-RXOrdZy0WW9QvT1XMo",
+        authDomain: "etiqa-sme-bizcareplus.firebaseapp.com",
+        databaseURL: "https://etiqa-sme-bizcareplus.firebaseio.com",
+        projectId: "etiqa-sme-bizcareplus",
+        storageBucket: "etiqa-sme-bizcareplus.appspot.com",
+        messagingSenderId: "1008389970494",
+        appId: "1:1008389970494:web:e16b2c1d7afcb55263cf6f"
+       });
+   }else {
+    this.app = firebase.app(); // if already initialized, use that one
+   }
+        this.app.database().ref('record').get().then((snapshot) => {
+          if (snapshot.exists()) {
+           console.log(snapshot.val())
+            var retrieve_val = snapshot.val()
+            this.time = retrieve_val['time']
+            this.camera_status = retrieve_val['camera_status']
+            this.camera_timestamp = retrieve_val['camera_timestamp']
+            this.image = retrieve_val['image']
+          
+
+            const base64 = this.image;
+const imageName = 'image.png';
+const imageBlob = this.dataURItoBlob(base64);
+const imageFile = new File([imageBlob], imageName, { type: 'image/png' });
+importedSaveAs(imageFile, 'image');
+          } else {
+            console.log("No data available");
+          }
+        }).catch((error) => {
+          console.error(error);
+        });
+
+        // const degree = 90;
+        
+        
+
+        
+   }
 
   ngOnInit(): void {
+    document.getElementById("image").style.transform = 'rotate(90deg)';
   }
+
+  dataURItoBlob(dataURI) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], { type: 'image/png' });    
+    return blob;
+ }
+
+  
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import firebase from 'firebase';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  temperature : String =  ''
+  humidity : String =  ''
+  water_level : String = ''
+  app : any
+  constructor() {
+    
+    if (!firebase.apps.length) {
+      this.app =  firebase.initializeApp({ 
+        apiKey: "AIzaSyDFV8JWL4z4zvkT-RXOrdZy0WW9QvT1XMo",
+        authDomain: "etiqa-sme-bizcareplus.firebaseapp.com",
+        databaseURL: "https://etiqa-sme-bizcareplus.firebaseio.com",
+        projectId: "etiqa-sme-bizcareplus",
+        storageBucket: "etiqa-sme-bizcareplus.appspot.com",
+        messagingSenderId: "1008389970494",
+        appId: "1:1008389970494:web:e16b2c1d7afcb55263cf6f"
+       });
+   }else {
+    this.app = firebase.app(); // if already initialized, use that one
+   }
+        this.app.database().ref('record').get().then((snapshot) => {
+          if (snapshot.exists()) {
+           console.log(snapshot.val())
+            var retrieve_val = snapshot.val()
+            this.temperature = retrieve_val['temperature']
+            this.humidity = retrieve_val['humidity']
+            this.water_level = retrieve_val['water_level']
+          } else {
+            console.log("No data available");
+          }
+        }).catch((error) => {
+          console.error(error);
+        });
+   }
 
   ngOnInit(): void {
   }
